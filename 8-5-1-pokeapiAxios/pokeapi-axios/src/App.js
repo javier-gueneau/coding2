94f9d0e85d400e94b607b471c486b3e159a4e97e
Javier Gueneau <javiergueneau@gmail.com>
2022-08-06 09:22:55 -0400

    Axios poke!

---
+++ 8-5-1-pokeapiAxios/pokeapi-axios/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import AxiosComponent from './components/AxiosComponent';
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
+
+const App = props=> {
+  
+  const[responseData,setResponseData]=useState([]);
+
+  console.log('en App');
+  
+  const Prueba =()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+      .then(response=>{
+        
+      console.log(response.data.results);
+      setResponseData(response.data.results)
+      })
+  }
+
+  return (
+    <div className="App">
+      {<button onClick={Prueba} >Fetch poke!</button>}
+      
+      {responseData.map((item)=>{
+        return(
+          <p key={item.name}>{item.name}</p>)}
+        )
+      }
+
+      <p>..</p>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ 8-5-1-pokeapiAxios/pokeapi-axios/src/components/AxiosComponent.js
@@ -0,0 +1,29 @@
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
+
+const AxiosComponent = props =>{
+
+    /* console.log('dd') */
+    /* useEffect(()=>{
+         axios.get('https://pokeapi.co/api/v2/pokemon')
+         .then(response=>{setResponseData(response.data)})
+    
+     },[]);
+
+    const[responseData,setResponseData]=useState(null); */
+
+    /* const UseEffect =()=>{
+    } */
+
+    return(
+        <div>
+            {/* {AxiosComponent} */}
+            axios Component aa dd
+            <button /* onClick={AxiosComponent} */ >bb</button>
+        </div>
+    )
+
+    }
+
+    export default AxiosComponent;
+
